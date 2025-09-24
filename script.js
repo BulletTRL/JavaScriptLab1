00ab4ea3309dbcdba2c721e236dfdd35e633bfb9
Tyler (Bullet) Hoyos <tylerhoyos2@gmail.com>
2025-09-23 22:49:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// Declaring the use of the variables
+
+let studentName = "Tyler Hoyos";
+let studentAge = 23;
+let isEnrolled = true;
+
+console.log("Student Name: ", studentName);
+console.log("Student's Age is: ", studentAge);
+console.log("Is Enrolled: ", isEnrolled);
+
+// Data Type Information
+
+let favoriteSubject = "Mathematics";
+console.log("The students favorite subject is: ", favoriteSubject);
+
+let studentAvg = 82.5;
+console.log("The students avg grade is: ", studentAvg);
+
+let isPassing = true;
+let Passing = ""
+
+if (isPassing = true) {
+    Passing = "Passing";
+} else {
+    Passing = "Failing"
+}
+console.log("The student has a ", passing, " grade");
+
+let classes = ["ENG101", "FDT102", "PRT101"];
+console.log("The student is currently enrolled in the following classes: ", classes);
+
+let homeProfessor = { firstName: "Mark", lastName: "Smith", age: 30};
+console.log("The students homeroom professor is: ", professor);
+
+// Use of Basic Operators
+
+console.log("Addition:", 5 + 3);
+console.log("Subtraction:", 10 - 2);
+console.log("Multiplication:", 4 * 6);
+console.log("Division:", 20 / 4);
+let x = 10, y = 20;
+console.log("Is x < y?", x < y);
+console.log("Is x > y?", x > y);
+console.log("Is x equal to 10?", x === 10);
+console.log("Is x not equal to y?", x !== y);
+let a = true, b = false;
+console.log("Logical AND:", a && b);
+console.log("Logical OR:", a || b);
+console.log("Logical NOT on a:", !a);
